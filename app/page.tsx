382ed726d74ba6fdbbc4670ea130136859cd87b7
devbekasyl <devbekasyl@gmail.com>
2026-05-22 11:34:44 +0500

    feat: simplify page component by redirecting to Russian version

--- app/page.tsx
+++ app/page.tsx
@@ -1,29 +1,5 @@
-import Nav from "./components/Nav";
-import Hero from "./components/Hero";
-import DemoStream from "./components/DemoStream";
-import ProblemSolution from "./components/ProblemSolution";
-import Onboarding from "./components/Onboarding";
-import Features from "./components/Features";
-import PricingPreview from "./components/PricingPreview";
-import Faq from "./components/Faq";
-import Contact from "./components/Contact";
-import Footer from "./components/Footer";
+import { redirect } from "next/navigation";
 
 export default function Page() {
-  return (
-    <>
-      <Nav />
-      <Hero />
-      <main>
-        <DemoStream />
-        <ProblemSolution />
-        <Onboarding />
-        <Features />
-        <PricingPreview />
-        <Faq />
-        <Contact />
-      </main>
-      <Footer />
-    </>
-  );
+  redirect("/ru");
 }
